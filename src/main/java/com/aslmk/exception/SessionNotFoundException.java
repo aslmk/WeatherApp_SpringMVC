96edb71f8a2a03fcea4398226c8658eb7634a951
aslmk <aulaensov@gmail.com>
2025-01-24 23:48:56 +0500

    Delete SessionNotFoundException

--- src/main/java/com/aslmk/exception/SessionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.exception;
-
-public class SessionNotFoundException extends  RuntimeException {
-    public SessionNotFoundException(String message) {
-        super(message);
-    }
-}

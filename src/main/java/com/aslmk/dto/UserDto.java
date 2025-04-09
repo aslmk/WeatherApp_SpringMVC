6da4f9c4f11a2f377c63dee85feed557d3589c38
aslmk <aulaensov@gmail.com>
2025-04-09 21:30:13 +0500

    feat: add validation to fields of UserDto

--- src/main/java/com/aslmk/dto/UserDto.java
+++ src/main/java/com/aslmk/dto/UserDto.java
@@ -1,11 +1,16 @@
 package com.aslmk.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 
 @Getter
 @Setter
 public class UserDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must contain at least from 3 to 20 symbols")
     private String username;
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }

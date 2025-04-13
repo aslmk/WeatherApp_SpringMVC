4190930e09b239d68c1cbcfeeea94c93cec903e7
aslmk <aulaensov@gmail.com>
2025-04-13 21:48:18 +0500

    refactor: add Builder, NoArgsConstructor and AllArgsConstructor annotations to Location entity

--- src/main/java/com/aslmk/model/Location.java
+++ src/main/java/com/aslmk/model/Location.java
@@ -1,8 +1,7 @@
 package com.aslmk.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -10,6 +9,9 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Table(name = "Locations")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
